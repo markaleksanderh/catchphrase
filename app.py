7efc406d4a656bbcdac9e68aa4d43683275d5be5
Mark Aleksander Hil <mark@100shapes.com>
2019-02-20 13:21:46 +0000

    Pulls gif from static

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
-gifs = ["Gif: " + str(i) for i in range(10)]
+gifs = ["supportive.gif"]+["Gif: " + str(i) for i in range(10)]
 
 @app.route('/<int:gif_num>')
 def catchphrase(gif_num):
