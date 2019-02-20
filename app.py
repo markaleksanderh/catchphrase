8377da45140321e5331bddf71d8f69122d593ae5
Mark Aleksander Hil <mark@100shapes.com>
2019-02-20 12:21:30 +0000

    Added next button

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ gifs = ["Gif: " + str(i) for i in range(10)]
 @app.route('/<int:gif_num>')
 def catchphrase(gif_num):
     catchphrase = "Example catchphrase"
-    return render_template('index.html', catchphrase=catchphrase, gif=gifs[gif_num])
+    return render_template('index.html', catchphrase=catchphrase, gif=gifs[gif_num], gif_num=gif_num)
