0c7e12ebeaaea09a67c0e6baf99768553dfc3625
Mark Aleksander Hil <mark@100shapes.com>
2019-02-21 12:17:04 +0000

    Added tile styling

--- static/js/tiles.js
+++ static/js/tiles.js
@@ -5,16 +5,26 @@ for (var i = 0; i < tiles.length; i++) {
   tileArr.push(tiles[i])
 }
 
-
 document.body.onkeyup = function(e){
   if(e.keyCode==32) {
     let i = Math.floor(Math.random()*tileArr.length)
     let tile = tileArr[i]
+    tile.style.background = "None"
     tile.style.backgroundColor = "rgba(0,0,0,0)"
     tileArr.splice(i, 1)
   }
 }
 
+
+
+
+// const delay = ms => new Promise(res=>setTimeout(res, ms))
+// const flashTile = async() => {
+//   await delay(1000)
+//   console.log("waited 1s")
+//   await delay(1000)
+//   console.log("wait additional 1s")
+// }
 
 
 
