7553c2c322ce9e298cab17150d805857211783ab
Mark Aleksander Hil <mark@100shapes.com>
2019-02-21 15:59:37 +0000

    Set up routes

---
+++ README.md
@@ -0,0 +1,8 @@
+## Order
+
+Index
+Studio
+Recruitment
+New business
+Game 0,1,2,3
+Any other business
--- app.py
+++ app.py
@@ -1,17 +1,40 @@
 from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
-gifs = ["placeholder.png", "supportive.gif", "cat.gif"]
+gifs = ["startslide.gif", "bbfc.gif", "sounds.gif", "tvl.gif"]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    title = "Mark + Lee WTF"
+    link_value = "studio"
+    return render_template('index.html', title=title, link_value=link_value)
+
+@app.route('/studio')
+def studio():
+    link_value = "recruitment"
+    title = "Studio updates"
+    return render_template('index.html', title=title, link_value=link_value)
+
+@app.route('/recruitment')
+def recruitment():
+    link_value = "nb"
+    title = "Recruitment updates"
+    return render_template('index.html', title=title, link_value=link_value)
+
+@app.route('/nb')
+def nb():
+    link_value = 0
+    title = "New business"
+    return render_template('index.html', title=title, link_value=link_value)
 
 @app.route('/<int:gif_num>')
 def game(gif_num):
     gif_len = len(gifs)
     return render_template('game.html', gif=gifs[gif_num], gif_num=gif_num, gif_len=gif_len)
 
+
 @app.route('/aob')
 def aob():
-    return render_template('aob.html')
+    title = "Any other business?"
+    return render_template('index.html', title=title)
