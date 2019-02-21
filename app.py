d4066dba6f3771b0ca1c3ec2f10cd6c322b8442e
Mark Aleksander Hil <mark@100shapes.com>
2019-02-21 13:52:01 +0000

    Retro font style, separated urls

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
-gifs = ["placeholder.png", "supportive.gif", "cat.gif"]+["Gif: " + str(i) for i in range(10)]
+gifs = ["placeholder.png", "supportive.gif", "cat.gif"]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 @app.route('/<int:gif_num>')
-def catchphrase(gif_num):
+def game(gif_num):
     gif_len = len(gifs)
-    return render_template('index.html', gif=gifs[gif_num], gif_num=gif_num, gif_len=gif_len)
+    return render_template('game.html', gif=gifs[gif_num], gif_num=gif_num, gif_len=gif_len)
+
+@app.route('/aob')
+def aob():
+    return render_template('aob.html')
