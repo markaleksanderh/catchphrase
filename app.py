d3489f85e1b1dff20d2848a29daef74ff0f468b0
Mark Aleksander Hil <mark@100shapes.com>
2019-02-21 16:39:00 +0000

    Added placeholder for images

--- app.py
+++ app.py
@@ -7,26 +7,30 @@ gifs = ["startslide.gif", "bbfc.gif", "sounds.gif", "tvl.gif", "csc.gif"]
 @app.route('/')
 def index():
     title = "Mark + Lee WTF"
+    image_link = None
     link_value = "studio"
-    return render_template('index.html', title=title, link_value=link_value)
+    return render_template('index.html', title=title, link_value=link_value, image_link=image_link)
 
 @app.route('/studio')
 def studio():
+    image_link = None
     link_value = "recruitment"
     title = "Studio updates"
-    return render_template('index.html', title=title, link_value=link_value)
+    return render_template('index.html', title=title, link_value=link_value, image_link=image_link)
 
 @app.route('/recruitment')
 def recruitment():
+    image_link = None
     link_value = "nb"
     title = "Recruitment updates"
-    return render_template('index.html', title=title, link_value=link_value)
+    return render_template('index.html', title=title, link_value=link_value, image_link=image_link)
 
 @app.route('/nb')
 def nb():
+    image_link = None
     link_value = 0
     title = "New business"
-    return render_template('index.html', title=title, link_value=link_value)
+    return render_template('index.html', title=title, link_value=link_value, image_link=image_link)
 
 @app.route('/<int:gif_num>')
 def game(gif_num):
@@ -35,12 +39,14 @@ def game(gif_num):
 
 @app.route('/aob')
 def aob():
+    image_link = None
     link_value = 'next'
     title = "Any other business?"
-    return render_template('index.html', title=title, link_value=link_value)
+    return render_template('index.html', title=title, link_value=link_value, image_link=image_link)
 
 @app.route('/next')
 def next_week():
+    image_link = None
     link_value = None
     title = "Next week: Chris + Sophie"
-    return render_template('index.html', title=title, link_value=link_value)
+    return render_template('index.html', title=title, link_value=link_value, image_link=image_link)
